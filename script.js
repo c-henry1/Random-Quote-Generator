ca895d621410ee50f4c3c3d8aac3b1621d8a5b13
c-henry1 <78730639+c-henry1@users.noreply.github.com>
2021-04-08 15:30:09 +0100

    Random Quote Generator

---
+++ script.js
@@ -0,0 +1,47 @@
+const newQuoteButton = document.querySelector("#js-new-quote");
+newQuoteButton.addEventListener('click', getQuote);
+
+const endpoint = "https://api.whatdoestrumpthink.com/api/v1/quotes/random"
+
+async function getQuote(){
+    spinner.classList.remove('hidden');
+    newQuoteButton.disabled = true;
+
+    try {
+        const response = await fetch(endpoint)
+        if (!response.ok) {
+            throw Error(response.statusText)
+        }
+
+        const json = await response.json();
+        displayQuote(json.message);
+        sendTweet(json.message);
+    }   catch (err) {
+        console.log(err)
+        alert('Failed to fetch new quote');
+    }   finally {
+        // enable the quote button
+        newQuoteButton.disabled = false;
+        // add the "hidden" class back again
+        spinner.classList.add('hidden');
+      }
+  
+}
+
+function displayQuote(quote) {
+    const quoteText= document.querySelector('#js-quote-text');
+    quoteText.textContent = quote;
+
+}
+
+const spinner = document.querySelector("#js-spinner");
+
+
+const twitterButton = document.querySelector("#js-tweet")
+
+function sendTweet(quote) {
+   twitterButton.setAttribute('href', `https://twitter.com/share?text=${quote} Donald Trump`);
+
+}
+
+getQuote();
